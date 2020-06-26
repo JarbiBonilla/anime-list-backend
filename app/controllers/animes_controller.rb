9825877721901f78dc431a1dbcc17a3ae0920e3c
JarbiBonilla <jarbibonilla@gmail.com>
2020-06-26 18:55:26 -0400

    added resources for animes and characters, created seed data for testing, and created an index action rendering json in my animes controller

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -1,2 +1,8 @@
 class AnimesController < ApplicationController
+
+    def index
+        @animes = Anime.all
+
+        render json: @animes.as_json(include: {characters: {only: [:name]}})
+    end
 end
